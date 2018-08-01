bad83db7975ab54101a7457fe9a5ac25db047847
suleymantekin <suleymantekin@users.noreply.github.com>
2018-08-01 21:21:40 +0300

    Add pause all action

--- src/store/actions/moods_actions.js
+++ src/store/actions/moods_actions.js
@@ -2,6 +2,7 @@
 export const TOGGLE_PLAY = 'TOGGLE_PLAY';
 export const CHANGE_VOLUME = 'CHANGE_VOLUME';
 export const PLAY_MOOD = 'PLAY_MOOD';
+export const PAUSE_ALL = 'PAUSE_ALL';
 
 export const togglePlay = (mood) => {
     return {
@@ -22,5 +23,10 @@ export const playMood = (mood) => {
         type: PLAY_MOOD,
         mood
     }
+}
+
+export const pauseAll = (moods) => {
+    type: PAUSE_ALL,
+        moods
 }
 
